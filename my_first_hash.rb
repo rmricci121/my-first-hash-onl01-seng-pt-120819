a241a98daf9fbc0a223c830326e2bae931e988f7
Robert M Ricci <rmricci121@gmail.com>
2019-12-15 21:45:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,11 +37,11 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+shipping_manifest["miskets"] => 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+shipping_manifest["gun powder"] => 4
 
   # return the shipping_manifest hash below
 
